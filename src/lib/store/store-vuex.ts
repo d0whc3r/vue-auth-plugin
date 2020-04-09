e04e1d9ee19b72318694082dc482cf5d16e5ee97
d0whc3r <d0whc3r@gmail.com>
2020-04-09 08:21:37 +0200

    fix: remove not used variable

--- src/lib/store/store-vuex.ts
+++ src/lib/store/store-vuex.ts
@@ -1,4 +1,4 @@
-import { ActionContext, Store } from 'vuex';
+import { ActionContext, MutationPayload, Store } from 'vuex';
 import { AuthUser, VueAuthStore } from '../../interfaces';
 import { IVueAuthOptions } from '../auth';
 
@@ -9,6 +9,8 @@ export type AuthVuexState = {
 
 export default class StoreVuex extends VueAuthStore {
   private readonly module?: string;
+  private readonly ACTION_SET_TOKEN = 'SET_TOKEN';
+  private readonly ACTION_SET_USER = 'SET_USER';
 
   constructor(Vue: any, options: IVueAuthOptions) {
     super(Vue, options);
@@ -22,15 +24,15 @@ export default class StoreVuex extends VueAuthStore {
   }
 
   public getRoles(): string[] {
-    return this.store.getters[`${this.module}/getRoles`];
+    return this.store.getters?.[`${this.module}/getRoles`];
   }
 
   public getToken(): string {
-    return this.store.getters[`${this.module}/getToken`];
+    return this.store.getters?.[`${this.module}/getToken`];
   }
 
   public getUser(): AuthUser {
-    return this.store.getters[`${this.module}/getUser`];
+    return this.store.getters?.[`${this.module}/getUser`];
   }
 
   public setToken(token: string | null): void {
@@ -50,44 +52,45 @@ export default class StoreVuex extends VueAuthStore {
         user: this.options.Vue.$data.user,
       } as AuthVuexState,
       mutations: {
-        SET_TOKEN(state: AuthVuexState, token: string) {
+        SET_TOKEN: (state: AuthVuexState, token: string) => {
           state.token = token;
         },
-        SET_USER(state: AuthVuexState, user: AuthUser) {
+        SET_USER: (state: AuthVuexState, user: AuthUser) => {
           state.user = user;
         },
       },
       actions: {
-        setToken(actionContext: ActionContext<AuthVuexState, any>, token: string) {
-          actionContext.commit('SET_TOKEN', token);
+        setToken: (actionContext: ActionContext<AuthVuexState, any>, token: string) => {
+          actionContext.commit(this.ACTION_SET_TOKEN, token);
         },
-        setUser(actionContext: ActionContext<AuthVuexState, any>, user: AuthUser) {
-          actionContext.commit('SET_USER', user);
+        setUser: (actionContext: ActionContext<AuthVuexState, any>, user: AuthUser) => {
+          actionContext.commit(this.ACTION_SET_USER, user);
         },
       },
       getters: {
-        getToken(state: AuthVuexState): string | undefined {
+        getToken: (state: AuthVuexState): string | undefined => {
           return state.token;
         },
-        getUser(state: AuthVuexState): AuthUser | undefined {
+        getUser: (state: AuthVuexState): AuthUser | undefined => {
           return state.user;
         },
-        getRoles(state: AuthVuexState): string[] {
+        getRoles: (state: AuthVuexState): string[] => {
           const user = state.user;
           return user && rolesVar && user[rolesVar];
         },
       },
     };
 
-    this.store.registerModule(this.module, module);
+    if (this.module) {
+      this.store.registerModule(this.module, module);
+    }
     // Listen for mutation from outside, e.g. with vuex-shared-mutations
-    this.store.subscribe((mutation: any, state: any) => {
-      if (mutation.type === `${this.module}/SET_TOKEN`
-        && mutation.payload !== this.options.Vue.$data.token) {
-        this.options.Vue.$data.token = mutation.payload;
-      } else if (mutation.type === `${this.module}/SET_USER`
-        && mutation.payload !== this.options.Vue.$data.user) {
-        this.options.Vue.$data.user = mutation.payload;
+    this.store.subscribe((mutation: MutationPayload) => {
+      const { payload, type } = mutation;
+      if (type === `${this.module}/${this.ACTION_SET_TOKEN}` && payload !== this.options.Vue.$data.token) {
+        this.options.Vue.$data.token = payload;
+      } else if (type === `${this.module}/${this.ACTION_SET_USER}` && payload !== this.options.Vue.$data.user) {
+        this.options.Vue.$data.user = payload;
       }
     });
   }
